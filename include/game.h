3ff090d9dcf73de64efe14a6ad46fdabb528bdc1
Estefania <alondra.hernandez5404@alumnos.udg.mx>
2026-06-25 20:32:31 -0600

    Escribi codigo de game.h y game.c

--- include/game.h
+++ include/game.h
@@ -2,3 +2,60 @@
 Enumeracion de contenido, 
 estructura de cajas,
 funciones publicas */
+
+#ifndef GAME_H
+#define GAME_H
+
+/*
+ * Tipos de contenido posibles dentro de una caja.
+ */
+typedef enum
+{
+    CHOCOLATES,
+    MENTAS,
+    MIXTO
+} Contenido;
+
+/*
+ * Representa una caja del acertijo.
+ */
+typedef struct
+{
+    Contenido etiqueta;
+    Contenido contenidoReal;
+} Caja;
+
+/*
+ * Genera aleatoriamente uno de los escenarios válidos.
+ */
+void generarEscenario(Caja cajas[]);
+
+/*
+ * Realiza una extracción de una caja.
+ */
+Contenido extraerDulce(Caja caja);
+
+/*
+ * Convierte un contenido a texto.
+ */
+const char *contenidoATexto(Contenido contenido);
+
+/*
+ * Compara la solución propuesta por el usuario
+ * con la solución real.
+ *
+ * Regresa:
+ * 1 -> Correcta
+ * 0 -> Incorrecta
+ */
+int validarSolucion(
+    Caja cajas[],
+    Contenido respuesta[]
+);
+
+/*
+ * Muestra la distribución real de las cajas.
+ */
+void mostrarSolucion(Caja cajas[]);
+
+#endif
--- src/game.c
+++ src/game.c
@@ -2,3 +2,61 @@
 Generar escenarios,
 validar solucion,
 mostrar solucion*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "../include/game.h"
+
+const char *contenidoATexto(Contenido contenido)
+{
+    switch (contenido)
+    {
+        case CHOCOLATES:
+            return "Chocolates";
+
+        case MENTAS:
+            return "Mentas";
+
+        case MIXTO:
+            return "Chocolates y Mentas";
+
+        default:
+            return "Desconocido";
+    }
+}
+void generarEscenario(Caja cajas[])
+{
+    cajas[0].etiqueta = CHOCOLATES;
+    cajas[1].etiqueta = MENTAS;
+    cajas[2].etiqueta = MIXTO;
+
+    int escenario = rand() % 2;
+
+    if (escenario == 0)
+    {
+        cajas[0].contenidoReal = MENTAS;
+        cajas[1].contenidoReal = MIXTO;
+        cajas[2].contenidoReal = CHOCOLATES;
+    }
+    else
+    {
+        cajas[0].contenidoReal = MIXTO;
+        cajas[1].contenidoReal = CHOCOLATES;
+        cajas[2].contenidoReal = MENTAS;
+    }
+}
+Contenido extraerDulce(Caja caja)
+{
+    if (caja.contenidoReal == CHOCOLATES)
+        return CHOCOLATES;
+
+    if (caja.contenidoReal == MENTAS)
+        return MENTAS;
+
+    if (rand() % 2 == 0)
+        return CHOCOLATES;
+
+    return MENTAS;
+}
